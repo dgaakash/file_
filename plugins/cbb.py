a7c7e54d996e316fd5047abf6bbeaadfaea9229e
Otakubhai <87870903+Otakubhai@users.noreply.github.com>
2023-11-08 13:09:49 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ My Owner : @Eren_is_Yeager\n○ Channel : @anime_channelz\n○ Support Group : @Anime_Chat_Lands</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ My Owner : @Call_Me_Blank\n○ Channel : @Otaku_Library\n○ Support Group : @Otaku_Chats</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
