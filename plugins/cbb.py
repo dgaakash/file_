6e1c9f07e891265a60cdc559e6e22f29329a22f6
dgaakash <138712879+dgaakash@users.noreply.github.com>
2024-01-21 14:54:41 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,5 @@
 #(©)Codexbotz
+#Recoded By @Its_Tartaglia_Childe
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,12 +11,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Aakash</a>\n○ My Channel : @anime_warior\n○ In Association with : @anime_warior_network\n○ Support : @Aakash1230</b>",
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n┃ 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 : <a href='https://t.me/Rokubotz'>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓</a>\n┃ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href='tg://user?id={OWNER_ID}'> 𝖶𝖺𝗂𝖿𝗎 </a>\n┃ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : <code>𝖯𝗒𝗍𝗁𝗈𝗇3</code>\n┃ 𝖫𝗂𝖻𝗋𝖺𝗋𝗒 : <a href='https://docs.pyrogram.org/'>𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆 𝖺𝗌𝗒𝗇𝖼𝗂𝗈 {__version__}</a>\n┃ 𝖲𝗈𝗎𝗋𝖼𝖾 𝖢𝗈𝖽𝖾 : <a href=https://t.me/Salazar5000>𝖳𝖺𝗅𝗄 𝖳𝗈 𝖧𝗂𝗆</a>\n┃ 𝖬𝖺𝗂𝗇 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 : <a href=https://t.me/animesilvervoid>​𝖲𝗂𝗅𝗏𝖾𝗋𝖵𝗈𝗂𝖽</a>\n┃ 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 : <a href=https://t.me/Haniflix>𝖧𝖺𝗇𝗂𝖥𝗅𝗂𝗑</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("𝖢𝗅𝗈𝗌𝖾", callback_data = "close")
                     ]
                 ]
             )
